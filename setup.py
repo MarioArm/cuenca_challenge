dfc10ddb63affde74584b97c886380f0edaf4177
Mario Armenta <89166862+MarioArm1@users.noreply.github.com>
2022-07-04 14:41:49 -0500

    Add travis CI to install and test project

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup
 
 with open('README.md', 'r') as f:
-   long_description = f.read()
+    long_description = f.read()
 
 setup(
-   name='cuenca_challenge',
-   version='1.0',
-   description='Cuenca challenge ',
-   long_description=long_description,
-   long_description_content_type='text/markdown',
-   url='https://github.com/MarioArm/cuenca-challenge',
-   author='Mario Armenta',
-   author_email='mario.armenta@outlook.com',
-   packages=['cuenca_challenge'],
-   install_requires=[],
+    name='cuenca_challenge',
+    version='1.0',
+    description='Cuenca challenge ',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/MarioArm/cuenca-challenge',
+    author='Mario Armenta',
+    author_email='mario.armenta@outlook.com',
+    packages=['src'],
+    install_requires=[
+        'pytest>=7.1.2',
+        'setuptools>=57.0.0'
+    ],
 )
